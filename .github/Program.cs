9d38e6d4ab0f57a2a536d662b1103044b57af697
Kopfkissen <kopfkissen.studios@gmail.com>
2026-06-15 16:36:23 +0200

    fixed new and print with empty statements, added initializeTurnier

--- .github/Program.cs
+++ .github/Program.cs
@@ -12,10 +12,7 @@ public class Program
             // intitialisiert Turniertabelle
         } else if (args[0].ToLower() == "print")
         {
-            foreach()
-            {
-                //Console.WriteLine("{} vs {}, Spiel-ID: {Spiel.id}");
-            }
+            
         } else if (args[0].ToLower() == "set")
         {
             
@@ -28,7 +25,11 @@ public class Program
         } else if (args[0].ToLower() == "result")
         {
             
+        } else if (args[0] == NULL)
+        {
+            Console.WriteLine("Ungültiger Befehl. Bitte verwenden Sie 'new', 'print', 'set', 'get', 'bid' oder 'result'.");
         }
+
     }
 }
 
@@ -48,12 +49,15 @@ public class Turniermanager
     }
     public void initializeTurnier()
     {
-        
-        
+        this.turniertabelle = new Turnier();
     }
     public void printSpiele()
     {
-        
+        Console.WriteLine("Aktuelle Spiele:");
+        foreach (var spiel in turniertabelle.GetAlleSpiele())
+        {            
+            Console.WriteLine($"{spiel.heimTeam.name} vs {spiel.auswaertsTeam.name}, Spiel-ID: {spiel.id}");
+        }
     }
     public void setQuote(int spielId, string typ, double quote);
     public double getQuote(int spielId, string typ);
